f017587c36a38520a4f0b52ebcb2a936954a760a
BotCelo <marcelo.aguiar.c@gmail.com>
2023-09-16 15:11:42 -0200

    add user

--- main.py
+++ main.py
@@ -1,9 +1,24 @@
 from src.service.service_user import ServiceUser
 
 service = ServiceUser()
+resposta = service.add_user('marcelo','test job 1')
+resposta = service.add_user('','test')
+
+#none
+resposta = service.add_user(None, 'test')
+resposta = service.add_user('test1', None)
+
+#tipo diferente de string
+resposta = service.add_user(5, 'job')
+resposta = service.add_user('marcelo', 5)
+
+resposta = service.add_user(True, 'job')
+resposta = service.add_user('marcelo', True)
+
 resposta = service.add_user('marcelo','test')
-resposta = service.add_user('marcelo2','test')
-resposta = service.add_user('marcelo','test')
+resposta = service.add_user('marcelo novo','test')
 
 
+print([x.name for x in service.store.bd])
+print([x.job for x in service.store.bd])
 print(resposta)
